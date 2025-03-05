025a9ac7796e7db29d2de900fbd95740b1690be4
EXyrus <164904383+EXyrus@users.noreply.github.com>
2025-03-05 10:37:42 +0100

    chore: update app info

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,27 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode }) => {
+  const isProdEnv = mode === "production";
+
+  return {
+
   server: {
-    host: "::",
-    port: 8080,
+    port: 3000,
+      host: isProdEnv ? "tabularasa.ng" : "tabularasa.internal",
+      allowedHosts: ["tabularasa.ng", "tabularasa.internal"],
   },
   plugins: [
     react(),
+    // sentryVitePlugin({
+    //   org: "xyrus-code",
+    //   project: process.env.npm_package_name,
+    //   telemetry: false,
+    //   release: {
+    //     name: `${process.env.npm_package_name}@${process.env.npm_package_version}`,
+    //   },
+    //   authToken: process.env.SENTRY_AUTH_TOKEN,
+    // }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
@@ -19,4 +33,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+}});
