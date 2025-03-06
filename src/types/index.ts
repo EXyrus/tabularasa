d75bfcd606d57b8e43a50efe676550893e5ef1b0
Prince Shammah <pshammah@epohcj.com>
2025-03-06 02:56:06 +0100

    refactor: remove unused components and update type exports

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,4 @@
-export * from './user';
-export * from './institution';
-export * from './responses';
+export * from "./app";
+export * from "./user";
+export * from "./institution";
+export * from "./responses";
--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,10 +1,10 @@
-export type User = {
-    id: string;
-    name: string;
-    email: string;
-    avatar?: string;
-    role: string;
-    appType: AppType;
-  }
+import { AppType } from "./app";
 
-  export type AppType = 'vendor' | 'institution' | 'guardian';
+export type User = {
+  id: string;
+  name: string;
+  email: string;
+  avatar?: string;
+  role: string;
+  appType: AppType;
+};
